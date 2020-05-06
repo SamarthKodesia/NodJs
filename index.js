0b13e104c0d4ad1a886bec7349e97c7056ffa24b
Samarth Kodesia <samarthkodesia@gmail.com>
2020-05-06 15:26:45 +0530

    Initialize

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log("Solving the rectangle with l = "+l+" and b = "+b);
+    if( l <=0 || b<=0 ){
+        console.log("Invalid dimensions");
+    }
+    else{
+        console.log("Area of the rectangle is "+ rect.area(l,b));
+        console.log("The perimeter of the rectangle is "+rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,6);
+solveRect(-3,4);
